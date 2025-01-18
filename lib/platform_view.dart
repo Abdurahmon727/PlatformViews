b826795265255c5b0c0affcba078f33ee133e7fd
Abdurahmon727 <abdurahmon727.uz@gmail.com>
2025-01-19 00:13:45 +0500

    feet: sample ui

---
+++ lib/platform_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlatformView extends StatelessWidget {
+  const PlatformView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
