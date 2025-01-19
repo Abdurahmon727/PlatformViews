b848868e37e9bfa5baae2504f8006bcde26be58e
Abdurahmon727 <abdurahmon727.uz@gmail.com>
2025-01-19 14:43:03 +0500

    feat: native view kotlin

--- android/app/src/main/kotlin/com/xda727/native_view_as_flutter_widget/native_view_as_flutter_widget/NativeView.kt
+++ android/app/src/main/kotlin/com/xda727/native_view_as_flutter_widget/native_view_as_flutter_widget/NativeView.kt
@@ -1,25 +1,48 @@
 package com.xda727.native_view_as_flutter_widget.native_view_as_flutter_widget
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Color
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.TextView
+import androidx.annotation.OptIn
+import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
 import io.flutter.plugin.platform.PlatformView
 
+@SuppressLint("ResourceType", "InflateParams")
+@OptIn(UnstableApi::class)
 internal class NativeView(context: Context, id: Int, creationParams: Map<String?, Any?>?) :
     PlatformView {
-    private val textView: TextView
+
+    private var content: View
+
+    private var playerView: PlayerView
+    private var player: ExoPlayer
 
     override fun getView(): View {
-        return textView
+        return content
     }
 
-    override fun dispose() {}
+    override fun dispose() {
+        player.release()
+    }
 
     init {
-        textView = TextView(context)
-        textView.textSize = 72f
-        textView.setBackgroundColor(Color.rgb(255, 255, 255))
-        textView.text = "Rendered on a native Android view (id: $id)"
+        content = LayoutInflater.from(context).inflate(R.layout.layout, null)
+        playerView = content.findViewById(R.id.playerView)
+        player = ExoPlayer.Builder(context).build().apply {
+            setMediaItem(MediaItem.Builder().apply {
+                setUri(
+                    "https://cdn.uzd.udevs.io/uzdigital/videos/bbc795b69faf3c4d1063777f2b220144/master.m3u8"
+                )
+            }.build())
+            prepare()
+            playWhenReady = true
+        }
+
+        playerView.player = player
+
     }
 }
